cae0dec22a0fc887002c5dff1b1c1bf560a92b2e
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-12-20 00:39:02 -0800

    build: backport a subset of changes from upstream

--- babel.config.js
+++ babel.config.js
@@ -4,36 +4,36 @@
 const targets = '>1% in US and not ie 11';
 
 module.exports = {
-    parserOpts: { strictMode: true },
-    plugins: [
-        '@babel/plugin-proposal-export-default-from',
-        '@babel/plugin-proposal-numeric-separator',
-        '@babel/plugin-proposal-throw-expressions',
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        '@babel/plugin-proposal-json-strings',
-        // * https://babeljs.io/blog/2018/09/17/decorators
-        // ? We're using the legacy proposal b/c that's what TypeScript wants
-        ['@babel/plugin-proposal-decorators', { legacy: true }],
-        '@babel/plugin-proposal-function-bind',
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-transform-typescript',
-    ],
-    env: {
-        // * Used by Jest and `npm test`
-        test: {
-            sourceMaps: 'both',
-            presets: [
-                ['@babel/preset-env', { targets: targets }],
-                ['@babel/preset-typescript', { allowDeclareFields: true }]
-            ]
-        },
-        // * Used by `npm run build`
-        production: {
-            presets: [
-                ['@babel/preset-env', { targets: targets }],
-                ['@babel/preset-typescript', { allowDeclareFields: true }]
-            ]
-        },
+  parserOpts: { strictMode: true },
+  plugins: [
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-throw-expressions',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-json-strings',
+    // * https://babeljs.io/blog/2018/09/17/decorators
+    // ? We're using the legacy proposal b/c that's what TypeScript wants
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    '@babel/plugin-proposal-function-bind',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-typescript'
+  ],
+  env: {
+    // * Used by Jest and `npm test`
+    test: {
+      sourceMaps: 'both',
+      presets: [
+        ['@babel/preset-env', { targets: targets }],
+        ['@babel/preset-typescript', { allowDeclareFields: true }]
+      ]
+    },
+    // * Used by `npm run build`
+    production: {
+      presets: [
+        ['@babel/preset-env', { targets: targets }],
+        ['@babel/preset-typescript', { allowDeclareFields: true }]
+      ]
     }
+  }
 };
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-    collectCoverageFrom: [
-        '**/*.{js,ts}',
-        '!**/*.d.ts',
-        '!**/node_modules/**'
-    ],
-    testEnvironment: 'node',
-    testRunner: 'jest-circus/runner',
-    verbose: false,
-    testPathIgnorePatterns: [ '/node_modules/' ],
-    setupFilesAfterEnv: [ 'jest-extended' ]
+  collectCoverageFrom: ['**/*.{js,ts}', '!**/*.d.ts', '!**/node_modules/**'],
+  testEnvironment: 'node',
+  testRunner: 'jest-circus/runner',
+  verbose: false,
+  testPathIgnorePatterns: ['/node_modules/'],
+  setupFilesAfterEnv: ['jest-extended']
 };
